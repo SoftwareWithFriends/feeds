a368427472a60384f51a0dc6f281a3d31fbe5266
Pete Zimbelman <pzimbelman@gmail.com>
2012-03-28 22:22:46 -0400

    messing with some styling. Added twitter bootstrap UI library

--- lib/feeds/posts.rb
+++ lib/feeds/posts.rb
@@ -5,7 +5,7 @@ module Feeds
     end
 
     def to_collection_hashes
-      @entries.map { |entry| {:title => entry.title, :body => entry.content } }
+      @entries.map { |entry| {:title => entry.title, :body => entry.content, :author => entry.authors.first.gsub("\n", "") } }
     end
   end
 end
