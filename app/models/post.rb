14d267af8d63cbc3649a83a8c9734726ea350b94
Pete Zimbelman <pzimbelman@gmail.com>
2012-04-04 21:37:41 -0400

    store updated_at and post_identifier values off of incoming posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,7 @@ class Post
   field :title, :type => String
   field :body, :type => String 
   field :author, :type => String
+  field :post_identifier, :type => String
+  field :updated_at, :type => DateTime
   belongs_to :subscription
 end
--- spec/models/feeds/posts_spec.rb
+++ spec/models/feeds/posts_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../../spec_helper'
 
 describe Feeds::Reader do
-  let(:entries) { [double("post_1", :title => "The title", :content => "the body", :author => "Pete Zimbelman"), 
-                   double("post_2", :title => "The second title", :content => "the second body", :author => "Mclovin")] }
-  let(:expected_collection) { [{:title => "The title", :body => "the body", :author => "Pete Zimbelman"}, 
-                               {:title => "The second title", :body => "the second body", :author => "Mclovin"}] }
+  let(:now) { Time.now }
+  let(:mock_entries) { [double("post_1", :title => "The title", :content => "the body", :author => "Pete Zimbelman", :id => "id1", :updated => now), 
+                       double("post_2", :title => "The second title", :content => "the second body", :author => "Mclovin", :id => "id2", :updated => now + 1.hour )]}
+  let(:expected_collection) { [{:title => "The title", :body => "the body", :author => "Pete Zimbelman", :post_identifier => "id1", :updated_at => now}, 
+                               {:title => "The second title", :body => "the second body", :author => "Mclovin", :post_identifier => "id2", :updated_at => now + 1.hour}] }
+
   it "should convert feed entries to be a collections hash prep'd for mongo" do
-    posts_presenter = Feeds::Posts.new(entries)
+    posts_presenter = Feeds::Posts.new(mock_entries)
     posts_presenter.to_collection_hashes.should == expected_collection
   end
-
 end
