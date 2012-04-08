50122270d067d44402a7648291e3d9aa29183aeb
ryan <mcgarvey.ryan@gmail.com>
2012-04-08 01:15:05 -0400

    [ryan] added show post back to subscription route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Feeds::Application.routes.draw do
   root :to => "posts#index"
   resources :subscriptions, :only => [:create, :index] do 
-    resources :posts, :only => [:index]
+    resources :posts, :only => [:index, :show]
   end
   resources :posts, :only => [:index, :show]
   resources :topics, :only => [:create]
