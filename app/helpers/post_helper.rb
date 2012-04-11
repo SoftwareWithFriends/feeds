1cdc9ab1b82603c88707c2fa43f802381700d880
Pete Zimbelman <pzimbelman@gmail.com>
2012-04-10 20:37:22 -0400

    UI tweaks

---
+++ app/helpers/post_helper.rb
@@ -0,0 +1,9 @@
+module PostHelper
+
+  def details_link_for(post)
+    date = post.published_at.strftime("%m/%d/%Y")
+    subscription_link = link_to("(#{post.subscription.url_base})", subscription_posts_path(post.subscription), 
+                                :title => "See more posts from this source", :class => "italic")
+    "on #{date} from #{subscription_link}".html_safe
+  end
+end
