d132b11626cae66469f59b9a3185913805df3865
Pete Zimbelman <pzimbelman@gmail.com>
2012-04-12 21:57:36 -0400

    remove VCR stuff as it does not work with feedzirra due to how it fetches feeds. will need to pick a new library or something soon

--- spec/integration/feeds_spec.rb
+++ spec/integration/feeds_spec.rb
@@ -3,47 +3,40 @@ require_relative '../spec_helper'
 describe "navigating the site", :js => true do
 
   it "should allow adding a feed and viewing just that feeds posts" do
-    VCR.use_cassette('feeds_spec') do
-      visit "/"
-      page.should have_content("Feeds")
-      fill_in "subscription_url", :with => "http://ambitiousapathy.com/feed"
-      expect {
-        click_on "Add"
-      }.to change(Post, :count).by(3)
+    visit "/"
+    page.should have_content("Feeds")
+    fill_in "subscription_url", :with => "http://ambitiousapathy.com/feed"
+    expect {
+      click_on "Add"
+    }.to change(Post, :count).by(4)
 
-      fill_in "subscription_url", :with => "http://feeds-test.herokuapp.com/feed"
-      expect {
-        click_on "Add"
-      }.to change(Post, :count).by(3)
-      page.should have_css(".post_row", :count => 6)
+    fill_in "subscription_url", :with => "http://feeds-test.herokuapp.com/feed"
+    expect {
+      click_on "Add"
+    }.to change(Post, :count).by(3)
+    page.should have_css(".post_row", :count => 7)
 
-      click_on "All Feeds"
-      click_on "Ambitious Apathy"
-      page.should have_css(".post_row", :count => 3)
-    end
+    click_on "All Feeds"
+    click_on "Ambitious Apathy"
+    page.should have_css(".post_row", :count => 4)
   end
 
   it "should show an error when trying to add an invalid feed" do
-    VCR.use_cassette('feeds_spec') do
-      visit "/"
-      fill_in "subscription_url", :with => "http://foobar"
-      click_on "Add"
-      page.should have_content("not a valid RSS/ATOM feed")
-    end
+    visit "/"
+    fill_in "subscription_url", :with => "http://foobar"
+    click_on "Add"
+    page.should have_content("not a valid RSS/ATOM feed")
   end
   
   it "should not create a new subscription" do
-    VCR.use_cassette('feeds_spec') do
-      visit "/"
-      fill_in "subscription_url", :with => "http://ambitiousapathy.com/feed"
-      expect {
-        click_on "Add"
-      }.to change(Subscription, :count).by(1)
-      fill_in "subscription_url", :with => "http://ambitiousapathy.com/feed"
-      expect {
-        click_on "Add"
-      }.to change(Subscription, :count).by(0)
-    end
+    visit "/"
+    fill_in "subscription_url", :with => "http://feeds-test.herokuapp.com/feed"
+    expect {
+      click_on "Add"
+    }.to change(Subscription, :count).by(1)
+    fill_in "subscription_url", :with => "http://feeds-test.herokuapp.com/feed"
+    expect {
+      click_on "Add"
+    }.to change(Subscription, :count).by(0)
   end
-  
 end
--- spec/integration/topic_spec.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../spec_helper'
-
-describe Topic do
-
-
-end
