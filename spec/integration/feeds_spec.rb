ad3ed2cc5132d565d2f1dd65420659823ee95fb9
Pete Zimbelman <pzimbelman@gmail.com>
2012-04-16 20:47:28 -0400

    fix bug by hiding save button when not logged in

--- spec/integration/feeds_spec.rb
+++ spec/integration/feeds_spec.rb
@@ -19,6 +19,10 @@ describe "navigating the site", :vcr, :js => true do
     click_on "All Feeds"
     click_on "Ambitious Apathy"
     page.should have_css(".post_row", :count => 4)
+    click_on "Feeds"
+    click_on "Testing feeds"
+    page.should have_content("Testing feeds")
+    page.should_not have_css("#save_post")
   end
 
   it "should show an error when trying to add an invalid feed" do
